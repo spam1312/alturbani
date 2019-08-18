249f24cd24c22ff7895f92207c9f1fbef0a22c8d
spam1312 <46295956+spam1312@users.noreply.github.com>
2019-08-18 22:35:40 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("612730625027342338")
+setInterval(function() {
+channel.send(`الترراابين ملوووك الفزعاااات`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
